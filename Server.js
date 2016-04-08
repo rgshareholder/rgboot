6653a090b5a99b82eac0a092377b42288331ef9c
Norbert <nxm@Joannas-iMac.nyc.rr.com>
2016-04-07 23:41:30 -0400

    port

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+var port = process.env.PORT || 8080;
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -28,6 +29,6 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(port,function(){
+  console.log('Our app is running on http://localhost:' + port);
 });
